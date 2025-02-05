37b8be58a736b3c7b1e0039da9b9db2ca5cc96be
FelipeRodrigues07 <feliperodriguesrs07@outlook.com>
2025-02-05 08:12:01 -0300

    feat: studying route groups in laravel

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,82 @@ Route::get('/', function () {    //fica dentro de resources dentro, welcome, bla
     return view('welcome');
 });
 
-Route::get('/empresa', function () {
-    return view('site/empresa');
+
+Route::any('/any', function(){
+    return "Permite todo tipo de acesso http (put,delete, get, post)";
 });
+
+Route::match(['get', 'post'], '/match', function () {
+    return "Permite apenas acessos definidos";
+});
+
+
+Route::get('/produto/{id}/{cat?}', function ($id, $cat = '') {
+    return "O ID do produto é: {$id}  <br> A categoria é {$cat}";
+});
+
+// Route::get('/sobre', function(){
+//     return redirect('/empresa');
+// });
+Route::redirect('/sobre', '/empresa');
+
+// Route::get('/empresa', function () {
+//     return view('site/empresa');
+// });
+Route::view('/empresa', '/site/empresa');
+
+
+Route::get('news', function(){
+   return view('news');
+})->name('noticias');
+
+Route::get('/novidades', function(){
+    return redirect()->route('noticias');
+});
+
+// Route::prefix('admin')->group(function(){
+//     Route::get('dashboard', function(){
+//         return "dashboard";
+//     });
+    
+//     Route::get('users', function(){
+//         return "users";
+//     });
+    
+//     Route::get('clientes', function(){
+//         return "clientes";
+//     });
+// });
+
+// Route::name('admin.')->group(function(){
+//     Route::get('admin/dashboard', function(){
+//         return "dashboard";
+//     })->name('dashboard');
+    
+//     Route::get('admin/users', function(){
+//         return "users";
+//     })->name('users');
+    
+//     Route::get('admin/clientes', function(){
+//         return "clientes";
+//     })->name('clientes');
+// });
+
+Route::get('/teste', function(){
+    return redirect()->route('admin.clientes');
+});
+
+Route::group(['prefix' => 'admin', 'as' => 'admin.'], function(){
+    Route::get('dashboard', function(){
+        return "dashboard";
+    })->name('dashboard');
+    
+    Route::get('users', function(){
+        return "users";
+    })->name('users');
+    
+    Route::get('clientes', function(){
+        return "clientes";
+    })->name('clientes');
+});
+
