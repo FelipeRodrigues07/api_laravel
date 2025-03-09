9999464b7a48d01c64b8f3097481756ba9929950
FelipeRodrigues07 <feliperodriguesrs07@outlook.com>
2025-03-09 15:27:10 -0300

    style: Dynamic categories with viewShare

--- resources/views/site/layout.blade.php
+++ resources/views/site/layout.blade.php
@@ -8,12 +8,28 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
 </head>
+<script>
+    function toggleDropdown() {
+        let dropdown = document.getElementById('dropdownCategorias');
+        dropdown.classList.toggle('hidden');
+    }
+</script>
 <body>  
     <nav class="bg-blue-600 text-white p-4">
-        <div class="container mx-auto flex justify-between items-center">
+        <div class="container mx-auto flex justify-between items-center px-8">
             <a href="#" class="text-xl font-bold">Curso Laravel</a>
             <ul class="flex space-x-4">
                 <li><a href="#" class="hover:underline">Home</a></li>
+                <li class="relative">
+                    <button onclick="toggleDropdown()" class="hover:underline flex items-center">
+                        Categorias <span class="material-icons ml-1 text-sm">expand_more</span>
+                    </button>
+                    <ul id="dropdownCategorias" class="absolute left-0 mt-2 w-48 bg-white text-black shadow-lg rounded-md hidden">
+                        @foreach ($categoriasMenu as $categoriaM)
+                        <li class="p-2 hover:bg-gray-200"><a href="#">{{ $categoriaM->nome}}</a></li>
+                        @endforeach
+                    </ul>
+                </li>
                 <li><a href="#" class="hover:underline">Carrinho</a></li>
             </ul>
         </div>
